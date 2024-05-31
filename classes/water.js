4fda721157345beb3faadc13cad95b35cf1addf7
Edison Chiu <ychi0518@uni.sydney.edu.au>
2024-05-31 13:43:19 +1000

    fixed water

--- classes/water.js
+++ classes/water.js
@@ -10,45 +10,25 @@ class Water{
         noStroke();
         let xoff = 0; // Option #1: 2D Noise
         // let xoff = yoff; // Option #2: 1D Noise
-        let steps = width / array.length;
-        // Iterate over horizontal pixels
-        for (let x = -width/2; x < width/2; x += steps) {
-            // Calculate a y value according to noise, map to
+        let steps = 0;
+        if(array>0){
             
-            // Option #1: 2D Noise
-            let y = map(noise(xoff, this.yoff), 0, 1, 60, 100);
-            counter +=1;
-      
-          // Option #2: 1D Noise
-          // let y = map(noise(xoff), 0, 1, 200,300);
-      
-          // Set the vertex
-          vertex(x, y);
-          // Increment x dimension for noise
-          xoff += 0.05;
+            steps = width / array.length;
+        } else {
+            steps = 10;
         }
-        // increment y dimension for noise
-        this.yoff += 0.01;
-        vertex(width/2,60);
-        vertex(width/2, height);
-        vertex(-width/2, height);
-        endShape(CLOSE);
-    }
-    display(array){
-        let counter =0;
-        fill(50,0,50,122);
-        // We are going to draw a polygon out of the wave points
-        beginShape();
-        noStroke();
-        let xoff = 0; // Option #1: 2D Noise
-        // let xoff = yoff; // Option #2: 1D Noise
-        let steps = width / array.length;
         // Iterate over horizontal pixels
         for (let x = -width/2; x < width/2; x += steps) {
             // Calculate a y value according to noise, map to
-            
+            let y = 0
             // Option #1: 2D Noise
-            let y = map(array[counter]/255, 0, 1, 60, 100);
+            if(array>0){
+
+                y = map(array[counter]/255, 0, 1, 60, 100);
+            } else{
+
+                y = map(noise(xoff, this.yoff), 0, 1, 60, 100);
+            }
             counter +=1;
       
           // Option #2: 1D Noise
